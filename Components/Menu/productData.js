6b86542fa17dc9f72497ac6b6c2765a2e0270404
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-28 22:04:13 +0530

    Create productData.js

---
+++ Components/Menu/productData.js
@@ -0,0 +1,40 @@
+const productData = [
+  {
+    id:1,
+    img:'src/Images/product1.jpg',
+    name:'Filter Coffee Powder',
+    price:'₹1234'
+  },
+  {
+    id:2,
+    img:'src/Images/product2.jpg',
+    name:'Coffee Powder',
+    price:'₹1234'
+  },
+  {
+    id:3,
+    img:'src/Images/product3.jpg',
+    name:'Flavoured Coffee Beans',
+    price:'₹1234'
+  },
+  {
+    id:4,
+    img:'src/Images/product4.jpg',
+    name:'Roasted Coffee Beans',
+    price:'₹1234'
+  },
+  {
+    id:5,
+    img:'src/Images/product5.jpg',
+    name:'Insulated Coffee Mug',
+    price:'₹1234'
+  },
+  {
+    id:6,
+    img:'src/Images/product6.jpg',
+    name:'Coffee Mug',
+    price:'₹1234'
+  }
+]
+
+export default productData;
