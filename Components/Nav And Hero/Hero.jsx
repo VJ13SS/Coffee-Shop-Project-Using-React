71e0cd1ca0d34990ca546717c7b5427f36a7d0be
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-28 22:08:16 +0530

    Create Hero.jsx

---
+++ Components/Nav And Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroImg from '/src/Images/Hero.jpg'
+
+export default function Hero(){
+  return (
+    <div className = 'Hero' id = 'hero-section'>
+      
+      <div className = 'Hero-text'>
+        <h1>FRESH COFFEE IN THE MORNING</h1>
+        <p>Grab The taste of the wide coffee varieties.Put your Indulgence and enjoy the moment..!</p>
+        <button>Get a Sip</button>
+      </div>
+    </div>
+  )
+}
