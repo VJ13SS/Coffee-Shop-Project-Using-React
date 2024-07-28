e14ee03205496e4df10666232356bed922ae42e7
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-07-28 22:02:45 +0530

    Create menuData.js

---
+++ Components/Menu/menuData.js
@@ -0,0 +1,40 @@
+const menuData =[
+  {
+    id:1,
+    name:'Cappuccino',
+    img:'src/Images/menu item5.jpeg',
+    price:'₹123'
+  },
+  {
+    id:2,
+    name:'Latte',
+    img:'src/Images/menu item3.jpeg',
+    price:'₹123'
+  },
+  {
+    id:3,
+    name:'Espresso',
+    img:'src/Images/menu item1.jpeg',
+    price:'₹123'
+  },
+  {
+    id:4,
+    name:'Mocha',
+    img:'src/Images/menuItem8.jpeg',
+    price:'₹123'
+  },
+  {
+    id:5,
+    name:'Regular',
+    img:'src/Images/menu item2.jpeg',
+    price:'₹123'
+  },
+  {
+    id:1,
+    name:'Americano',
+    img:'src/Images/menu item6.jpeg',
+    price:'₹123'
+  }
+]
+
+export default menuData;
